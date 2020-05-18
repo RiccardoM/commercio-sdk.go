12ff8c8045c92f592c1b1818dda4ccd78b1ae272
Gianguido Sorà <me@gsora.xyz>
2020-05-18 10:58:46 +0200

    physically move the fucntion in the init file

--- init.go
+++ init.go
@@ -1,6 +1,32 @@
 package commercio
 
+import "github.com/cosmos/cosmos-sdk/types"
+
 // We use init() to configure commercionetwork's Cosmos settings, otherwise address/codec won't work
 func init() {
 	setCosmosConfig()
+}
+
+// setCosmosConfig sets up the Commercio.network HRP for Cosmos-SDK function calling.
+func setCosmosConfig() {
+	// this bit comes directly from commercio.network app/app.go.
+	bech32MainPrefix := "did:com:"
+
+	prefixValidator := "val"
+	prefixConsensus := "cons"
+	prefixPublic := "pub"
+	prefixOperator := "oper"
+
+	bech32PrefixAccAddr := bech32MainPrefix
+	bech32PrefixAccPub := bech32MainPrefix + prefixPublic
+	bech32PrefixValAddr := bech32MainPrefix + prefixValidator + prefixOperator
+	bech32PrefixValPub := bech32MainPrefix + prefixValidator + prefixOperator + prefixPublic
+	bech32PrefixConsAddr := bech32MainPrefix + prefixValidator + prefixConsensus
+	bech32PrefixConsPub := bech32MainPrefix + prefixValidator + prefixConsensus + prefixPublic
+
+	config := types.GetConfig()
+	config.SetBech32PrefixForAccount(bech32PrefixAccAddr, bech32PrefixAccPub)
+	config.SetBech32PrefixForValidator(bech32PrefixValAddr, bech32PrefixValPub)
+	config.SetBech32PrefixForConsensusNode(bech32PrefixConsAddr, bech32PrefixConsPub)
+	config.Seal()
 }
--- types.go
+++ types.go
@@ -8,30 +8,6 @@ import (
 	"github.com/cosmos/cosmos-sdk/x/bank"
 )
 
-// setCosmosConfig sets up the Commercio.network HRP for Cosmos-SDK function calling.
-func setCosmosConfig() {
-	// this bit comes directly from commercio.network app/app.go.
-	bech32MainPrefix := "did:com:"
-
-	prefixValidator := "val"
-	prefixConsensus := "cons"
-	prefixPublic := "pub"
-	prefixOperator := "oper"
-
-	bech32PrefixAccAddr := bech32MainPrefix
-	bech32PrefixAccPub := bech32MainPrefix + prefixPublic
-	bech32PrefixValAddr := bech32MainPrefix + prefixValidator + prefixOperator
-	bech32PrefixValPub := bech32MainPrefix + prefixValidator + prefixOperator + prefixPublic
-	bech32PrefixConsAddr := bech32MainPrefix + prefixValidator + prefixConsensus
-	bech32PrefixConsPub := bech32MainPrefix + prefixValidator + prefixConsensus + prefixPublic
-
-	config := types.GetConfig()
-	config.SetBech32PrefixForAccount(bech32PrefixAccAddr, bech32PrefixAccPub)
-	config.SetBech32PrefixForValidator(bech32PrefixValAddr, bech32PrefixValPub)
-	config.SetBech32PrefixForConsensusNode(bech32PrefixConsAddr, bech32PrefixConsPub)
-	config.Seal()
-}
-
 // messageEnclosure encloses a Cosmos message into its REST-accepted enclosure.
 type messageEnclosure struct {
 	Type  string      `json:"type"`
